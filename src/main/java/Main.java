b9d2091b303f0bf469f2475139f2256e0289b547
alexrsing <alex@sing.email>
2025-04-25 16:28:17 -0700

    Framework changed to Social App

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,12 @@
+/**
+ * Entry point for the program.
+ */
 public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
 
+    public void run() {
+
+    }
 }
