e8e0740528b1340488d75ae4f1ea4192b2113bf1
Glasteroid <kennethangeles880@yahoo.com>
2025-04-26 13:59:44 -0700

    minor stuff

--- app/models/location.py
+++ app/models/location.py
@@ -46,11 +46,11 @@ def get_place_details(place_id):
     # gets 'result' field from details dictionary representing parsed results from .place() function response, otherwise returns empty dictionary if not found
     return details.get('result', {})
 
-def search_and_upload_places():
+def search_and_upload_places(city, prompt):
     try:
-        city = input("City: ")
+        # city = input("City: ")
         lat, lng = convert_coordinates(city)
-        prompt = input("What would you like to search for: ")
+        # prompt = input("What would you like to search for: ")
         results = get_places(50, lat, lng, prompt)
         
         if not results or 'results' not in results:
